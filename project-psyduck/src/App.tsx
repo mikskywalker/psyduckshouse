8e42ef8884a40bf21a8936488f3cd384d25d86bb
Miks <michael@smudge.com>
2023-04-08 21:39:42 +0800

    major changes

---
+++ project-psyduck/src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Spline from "@splinetool/react-spline";
+import { Grid, Typography, makeStyles } from "@mui/material";
+
+function App() {
+  return (
+    <Grid
+      container
+      direction='row'
+      xs={12}
+      justifyContent="center"
+      alignItems="center"
+      className="App"
+    >
+      <Grid item xs={11} className="Psy" style={{ height: "90vh" }}>
+        <Spline scene="https://prod.spline.design/ekqlJiX42Sqf79M3/scene.splinecode" />
+      </Grid>
+      <Grid item xs={10}>
+        <h2 style={{textAlign: 'center'}}>&#169; Miks Casal</h2>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
